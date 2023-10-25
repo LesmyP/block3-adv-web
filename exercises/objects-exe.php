f5ac5646dbdb950853c07cae40a74b76681fa673
LesmyP <lesmyperez@gmail.com>
2023-10-25 17:07:52 -0400

    update objects

--- exercises/objects-exe.php
+++ exercises/objects-exe.php
@@ -4,7 +4,30 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-class ledlights
+class LedLights
+{
+    public $power; 
+    public $color;
+    public $brightness;
+
+}
+
+$powerButton = New TurnOnOff();
+
+public function TurnOnOff()
+{
+    $this->power = true;
+    echo "Turning on";
+}
+
+public function TurnOff()
+{
+    $this->power = false;
+    echo "Turning off";
+}
+
+
+
 
 
 ?>
