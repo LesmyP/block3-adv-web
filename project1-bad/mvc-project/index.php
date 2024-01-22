a5a0da252d2f2be50c4f998bb806a299ef0fd23b
LesmyP <lesmyperez@gmail.com>
2024-01-22 07:42:38 -0500

    cleaned files, rename old project folders

--- project1-bad/mvc-project/index.php
+++ project1-bad/mvc-project/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MCV Project - Lesmy Perez</title>
+    <title>MCV Project-not working - Lesmy Perez</title>
     <link rel="stylesheet" href="style.css">
     
 </head>
