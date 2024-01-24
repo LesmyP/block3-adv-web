ec9b8514c1a23815299c1d8c00f0defdd507bc6e
LesmyP <lesmyperez@gmail.com>
2024-01-23 21:49:16 -0500

    working code, commiting before starting new changes on partypes files

--- project1/mvc-project/views/brandform.php
+++ project1/mvc-project/views/brandform.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brand Form</title>
-    <!-- <link rel="stylesheet" href="style.css"> -->
+    <!-- <link rel="stylesheet" href="./style.css"> -->
 </head>
+
+<style>
+
+</style>
+
+
 <body>
 <div class="container">
 
@@ -14,8 +20,8 @@
     <form method="POST" action="?action=add">
         <input type="text" name="brandName" placeholder="Brand" value="<?php echo isset($_POST['brandName']) ? $_POST['brandName'] : ''; ?>">
 
-        <input type="submit" name="submit" value="Submit">
-        <input type="reset" name="reset" value="Reset">
+        <input class="btn" type="submit" name="submit" value="Submit">
+        <input class="btn" type="reset" name="reset" value="Reset">
 
     </form>
 </div>
